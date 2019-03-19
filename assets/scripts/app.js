c0bc375ce33a3e7b6d6530caf78a36b1619a42b0
Amillz70 <adammiller7@yahoo.com>
2019-03-19 14:36:43 -0400

    Continue work on form fields
    Basic outline of fields in place
    Start work on email confirmation
    Separate api calls from app.js file

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,20 +7,20 @@
 // require('./example')
 
 // $(() => {
-import wixData from 'wix-data';
-import wixLocation from 'wix-location';
-import wixWindow from 'wix-window';
-import {getCIDLicense} from 'backend/isgro-order';
+import wixData from 'wix-data'
+import wixLocation from 'wix-location'
+import wixWindow from 'wix-window'
+import {getCIDLicense} from 'backend/isgro-order'
 
 $w.onReady(function () {
 
     if(wixWindow.rendering.renderCycle === 2) {
         //This part is to retrieve the data from the wix db
-        let path = wixLocation.path;
-        let query = wixLocation.query;
+        const path = wixLocation.path;
+        const query = wixLocation.query;
         let collectionName = 'Default';
 
-        if ("collection" in query) {
+        if ('collection' in query) {
 
             collectionName = query['collection'];
 
@@ -28,15 +28,15 @@ $w.onReady(function () {
 
         let objNew = {};
 
-        if ("data" in query) {
-            let obj = JSON.parse(atob(query['data']));
+        if ('data' in query) {
+            const obj = JSON.parse(atob(query['data']));
 
-            for (let prop in obj) {
-                let newProp = prop.replace('.', '');
+            for (const prop in obj) {
+                const newProp = prop.replace('.', '');
                 objNew[newProp] = obj[prop];
             }
 
-            //console.log('Sending 123FormBuilder submitted data to WixCode');
+            // console.log('Sending 123FormBuilder submitted data to WixCode');
 
             let dataSent = wixData.insert(collectionName, objNew);
         } else {
@@ -57,9 +57,9 @@ $w.onReady(function () {
             subseller = 'verizon3';
         }
 
-        let custEmail = objNew['Email'].toLowerCase();
+        const custEmail = objNew['Email'].toLowerCase();
 
-        let licenseObj ={
+        const licenseObj ={
 
             subSellerId: subseller,
 
@@ -91,16 +91,16 @@ $w.onReady(function () {
 
         if (objNew['Phone']) licenseObj.phone = objNew['Phone'];
 
-        //uncomment to see licenseObj contents
-        //console.log('Contents of licenseObj:');
-        //for (let value in licenseObj){
+        // uncomment to see licenseObj contents
+        // console.log('Contents of licenseObj:');
+        // for (let value in licenseObj){
         //  console.log(value + ' =  ' + licenseObj[value]);
-        //}
-        //run the call
+        // }
+        // run the call
 
         let promise = getCIDLicense(licenseObj);
 
-        promise.then(function(results){
+        promise.then(function(results) {
             //stupid javascript
             if(results && results.status){
                 if(results.status == 200 || results.status == 201){
@@ -119,126 +119,91 @@ $w.onReady(function () {
                 console.log('Error: Occurred before sending API call');
                 console.log(results);
                 $w('#errorMessage').show();
-            }
-        });
-    }
-});
-
+      }
+    })
+  }
+})
 
 
 // Which references this code:
 // Seperate file
 
-import {fetch} from 'wix-fetch';
-
-export function getCIDLicense(licenseObj) {
-
-
-    //check for required fields
-
-    if (!licenseObj.subSellerId)return ('Error: subSellerId is missing');
-
-    if (!licenseObj.email)return ('Error: email Field is missing');
-
-    if (!licenseObj.sku)return ('Error: sku Field is missing');
-
-    if(!licenseObj.merchantId)return ('Error: merchantId Field is missing');
-
-    if(!licenseObj.merchantName)return ('Error: merchantName Field is missing');
-
-    //create body object
-
-    var data = {
-
-        subSellerId: licenseObj.subSellerId,
-
-        adminFirstName: licenseObj.firstName,
-
-        adminLastName: licenseObj.lastName,
-
-        adminEmail: licenseObj.email,
-
-        sku: licenseObj.sku,
-
-        merchantId: licenseObj.merchantId,
-
-        merchantName: licenseObj.merchantName
-
-    };
-
-    //optional fields
-
-    if (licenseObj.address) data.address = licenseObj.address;
-
-    if (licenseObj.zip) data.zip = licenseObj.zip;
-
-    if (licenseObj.state) data.state = licenseObj.state;
-
-    if (licenseObj.country) data.country = licenseObj.country;
-
-    if (licenseObj.phone) data.phone = licenseObj.phone;
-
-
-
-    //Middleware API URL (dev or prod)
-
-    //dev var url = 'https://dev-acs-v1.cyberidlock.com/api/cidlicense';
-
-    var url = 'https://acs-v1.endpointlock.com/api/cidlicense';
-
-    //uncomment this to return the raw request body for debugging
-
-    //return (JSON.stringify(data));
-
-    //Run the API command
-
-    let promise = fetch(url, {
-
-        body: JSON.stringify(data),
-
-        //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-
-        //credentials: 'same-origin', // include, same-origin, *omit
-
-        headers: {
-
-            'Authorization': 'Bearer gYEt1ZbJmcXAnB8Vtpe50KymwMu1BNLFcTV0lRB3u97HOXz4GR',
-
-            'X-AdvancedCyberSecurity-SellerId': 'acs-isgro',
-
-            'content-type': 'application/json'
-
-        },
-
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-
-        //mode: 'no-cors', // no-cors, cors, *same-origin
-
-        //redirect: 'follow', // *manual, follow, error
-
-        //referrer: 'no-referrer', // *client, no-referrer
-
-        })
-
-        .then(function (response) {
-
-            //gotta return twice because stupid promises
-
-            return (response.json());
-
-        })
-
-        .catch(function (error) {
-
-            console.log('Error during post');
-
-            return ('Error: The API Call Failed.');
-
-        });
-
-
-
-    return (promise);
-
-
-})
+// import {fetch} from 'wix-fetch';
+//
+// export function getCIDLicense(licenseObj) {
+//
+//
+//     //check for required fields
+//
+//     if (!licenseObj.subSellerId)return ('Error: subSellerId is missing');
+//
+//     if (!licenseObj.email)return ('Error: email Field is missing');
+//
+//     if (!licenseObj.sku)return ('Error: sku Field is missing');
+//
+//     if(!licenseObj.merchantId)return ('Error: merchantId Field is missing');
+//
+//     if(!licenseObj.merchantName)return ('Error: merchantName Field is missing');
+//
+//     //create body object
+//
+//     var data = {
+//
+//         subSellerId: licenseObj.subSellerId,
+//
+//         adminFirstName: licenseObj.firstName,
+//
+//         adminLastName: licenseObj.lastName,
+//
+//         adminEmail: licenseObj.email,
+//
+//         sku: licenseObj.sku,
+//
+//         merchantId: licenseObj.merchantId,
+//
+//         merchantName: licenseObj.merchantName
+//
+//     };
+//     //optional fields
+//
+//     if (licenseObj.address) data.address = licenseObj.address;
+//
+//     if (licenseObj.zip) data.zip = licenseObj.zip;
+//
+//     if (licenseObj.state) data.state = licenseObj.state;
+//
+//     if (licenseObj.country) data.country = licenseObj.country;
+//
+//     if (licenseObj.phone) data.phone = licenseObj.phone;
+//
+//     //Middleware API URL (dev or prod)
+//     //dev var url = 'https://dev-acs-v1.cyberidlock.com/api/cidlicense';
+//     var url = 'https://acs-v1.endpointlock.com/api/cidlicense';
+//     //uncomment this to return the raw request body for debugging
+//     //return (JSON.stringify(data));
+//     //Run the API command
+//     let promise = fetch(url, {
+//         body: JSON.stringify(data),
+//         //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+//         //credentials: 'same-origin', // include, same-origin, *omit
+//         headers: {
+//             'Authorization': 'Bearer gYEt1ZbJmcXAnB8Vtpe50KymwMu1BNLFcTV0lRB3u97HOXz4GR',
+//             'X-AdvancedCyberSecurity-SellerId': 'acs-isgro',
+//             'content-type': 'application/json'
+//         },
+//         method: 'POST', // *GET, POST, PUT, DELETE, etc.
+//         //mode: 'no-cors', // no-cors, cors, *same-origin
+//         //redirect: 'follow', // *manual, follow, error
+//         //referrer: 'no-referrer', // *client, no-referrer
+//         })
+//
+//         .then(function (response) {
+//             //gotta return twice because stupid promises
+//             return (response.json());
+//         })
+//         .catch(function (error) {
+//             console.log('Error during post');
+//             return ('Error: The API Call Failed.');
+//         });
+//     return (promise);
+// })
