e3b623c4e10d8dff80cf9ec12d3a42668c876593
Amillz70 <adammiller7@yahoo.com>
2019-03-20 12:18:53 -0400

    Make error message logic in index.html
    TODO- test functionality of app.js, events.js, and ui.js

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -2,6 +2,7 @@
 
 const api = require('./api.js')
 const ui = require('./ui.js')
+// const getFormFields = require('../../lib/get-form-fields.js')
 
 const onSubmit = function (event) {
   event.preventDefault()
--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -1,11 +1,15 @@
 'use strict'
 
 const submitSuccess = function () {
-
+  '#display-message'.html('test')
 }
 
 const submitFailure = function () {
-// individual failure notices?
+  // individual failure notices?
+  // if ('#firstname' === '') {
+  //   $('#display-message').html('Please enter a First Name')
+  // }
+  '#display-message'.html('bad test')
 }
 
 module.exports = {
