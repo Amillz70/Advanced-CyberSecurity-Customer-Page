14cdfc72e3c44d5f4cdc8444e61c99559236aac2
Amillz70 <adammiller7@yahoo.com>
2019-03-21 14:42:29 -0400

    Add line to html to signify which fields are required
    Change file type for onsubmit curl script

---
+++ curl-scripts/onsubmit.sh
@@ -0,0 +1,5 @@
+curl "https://acs-v1.endpointlock.com/api/cidlicense" \
+--include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
