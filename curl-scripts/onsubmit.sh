9bdba6d6174d2328a7020f96f8d1bff89e89e18e
Amillz70 <adammiller7@yahoo.com>
2019-03-21 16:30:37 -0400

    Functional curl script for submit

--- curl-scripts/onsubmit.sh
+++ curl-scripts/onsubmit.sh
@@ -3,12 +3,13 @@ curl "https://acs-v1.endpointlock.com/api/cidlicense" \
   --request POST \
   --header "Content-Type: application/json" \
   --header "Authorization: Bearer gYEt1ZbJmcXAnB8Vtpe50KymwMu1BNLFcTV0lRB3u97HOXz4GR" \
-  --header "X-AdvancedCyberSecurity-SellerId": 'acs-isgro' \
+  --header "X-AdvancedCyberSecurity-SellerId: acs-isgro" \
   --data '{
-    "customer": {
-      "first_name": "'"${FIRST}"'",
-      "last_name": "'"${LAST}"'",
-      "email": "'"${EMAIL}"'",
-      "repeat_email": "'"${EMAIL}"'"
-    }
+      "subSellerId": "'"${VERIZON}"'",
+      "merchantId": "'"${EMAIL}"'",
+      "merchantName": "'"${MERCHANT}"'",
+      "adminFirstName": "'"${FIRST}"'",
+      "adminLastName": "'"${LAST}"'",
+      "adminEmail": "'"${EMAIL}"'",
+      "sku": "CYBERC_A"
   }'
