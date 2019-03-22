b78024d9d65d115c6fd8ac9e370ae5db76ee799b
Amillz70 <adammiller7@yahoo.com>
2019-03-22 13:43:05 -0400

    Create new invoice number and repeat invoice numb form for Merchant Name
    Create alert if invoice numbers don't match
    Remove MerchantId file logic because no longer needed

--- assets/scripts/merchantId.js
+++ assets/scripts/merchantId.js
@@ -1,22 +1,22 @@
-'use strict'
-
-const html = require('../../index.html')
-
-const merchantId = function () {
-  const newId = []
-  const firstname = []
-  const lastname = []
-
-  '#firstname'.push
-
-  if ('#firstname'.length + '' + '#lastname'.length > 32) {
-    push()
-  }
-}
-// if firstname.value + '' + lastname.value === 32
-// slice -32
-// return []
-
-module.exports {
-  merchantId
-}
+// 'use strict'
+//
+// const html = require('../../index.html')
+//
+// const merchantName = function () {
+//   const newId = []
+//   const firstname = [].
+//   const lastname = []
+//
+//   '#firstname'.push
+//
+//   if ('#firstname'.length + '' + '#lastname'.length > 32) {
+//     push()
+//   }
+// }
+// // if firstname.value + '' + lastname.value === 32
+// // slice -32
+// // return []
+//
+// module.exports = {
+//   merchantName
+// }
