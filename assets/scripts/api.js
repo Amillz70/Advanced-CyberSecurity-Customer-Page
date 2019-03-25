ee334fd7674d725a471d4bb1e43263a7e7523e8b
Amillz70 <adammiller7@yahoo.com>
2019-03-25 11:27:52 -0400

    Hard code url in api file
    update console log to see when deployed

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -1,12 +1,13 @@
 'use strict'
 
-const config = require('./config.js')
+// const config = require('./config.js')
 
 const newCustomer = (customerData) => {
   return $.ajax({
-    'Access-Control-Allow-Origin': config.apiUrl,
-    method: 'POST',
-    'Access-Control-Allow-Headers': {
+  //  url: config.apiUrl,
+    url: 'https://acs-v1.endpointlock.com/api/cidlicense',
+    method: 'PUT',
+    headers: {
       'Authorization': 'Bearer gYEt1ZbJmcXAnB8Vtpe50KymwMu1BNLFcTV0lRB3u97HOXz4GR',
       'X-AdvancedCyberSecurity-SellerId': 'acs-isgro'
     },
