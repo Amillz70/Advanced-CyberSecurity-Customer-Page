12cdb6174e5fcc3bf3547abec86811464cf87b73
Amillz70 <adammiller7@yahoo.com>
2019-03-25 10:49:18 -0400

    Change to 'Access-Control-Allow-Methods' and 'Headers' in api

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -5,8 +5,8 @@ const config = require('./config.js')
 const newCustomer = (customerData) => {
   return $.ajax({
     'Access-Control-Allow-Origin': config.apiUrl,
-    method: 'POST',
-    headers: {
+    'Access-Control-Allow-Methods': 'POST',
+    'Access-Control-Allow-Headers': {
       'Authorization': 'Bearer gYEt1ZbJmcXAnB8Vtpe50KymwMu1BNLFcTV0lRB3u97HOXz4GR',
       'X-AdvancedCyberSecurity-SellerId': 'acs-isgro'
     },
