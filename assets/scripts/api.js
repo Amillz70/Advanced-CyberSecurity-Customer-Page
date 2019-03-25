24a35eff29fe31f120d6c131dd47e667e22fd127
Amillz70 <adammiller7@yahoo.com>
2019-03-25 10:39:23 -0400

    Change url to 'Access-Control-Allow-Origin' in api.js file
    Receive a 404 error from it
    Will deploy to test if it works deployed
    Change dev url in config file

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -4,7 +4,7 @@ const config = require('./config.js')
 
 const newCustomer = (customerData) => {
   return $.ajax({
-    url: config.apiUrl,
+    'Access-Control-Allow-Origin': config.apiUrl,
     method: 'POST',
     headers: {
       'Authorization': 'Bearer gYEt1ZbJmcXAnB8Vtpe50KymwMu1BNLFcTV0lRB3u97HOXz4GR',
--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,8 +3,8 @@
 let apiUrl
 const apiUrls = {
   production: 'https://acs-v1.endpointlock.com/api/cidlicense',
-  development: 'http://localhost:4741'
-  // development: 'https://dev-v1.endpointlock.com/api/cidlicense'
+  // development: 'http://localhost:4741'
+  development: 'https://dev-v1.endpointlock.com/api/cidlicense'
 }
 
 if (window.location.hostname === 'localhost') {
