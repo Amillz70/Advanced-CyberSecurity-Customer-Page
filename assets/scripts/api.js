1f52ebd0cef1287faafa6b7b049760f375aae6f1
Amillz70 <adammiller7@yahoo.com>
2019-03-28 09:47:57 -0400

    Test to see if website passes authentication when deployed
    Update console.log to see when website deployed

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -4,18 +4,41 @@ const config = require('./config.js')
 
 const newCustomer = (customerData) => {
   customerData.sku = 'CYBERC_A'
+  console.log(customerData)
 
   return fetch(config.apiUrl, {
     method: 'POST',
     mode: 'no-cors',
-    headers: {
+    credentials: 'include',
+    headers: new Headers({
+      'Content-Type': 'application/json',
       'Authorization': 'Bearer gYEt1ZbJmcXAnB8Vtpe50KymwMu1BNLFcTV0lRB3u97HOXz4GR',
-      'X-AdvancedCyberSecurity-SellerId': 'acs-isgro',
-      'Content-Type': 'application/json'
-    },
+      'X-AdvancedCyberSecurity-SellerId': 'acs-isgro'
+    }),
     body: JSON.stringify(customerData)
   })
+    .then(response => response.json())
 }
+
+console.log(fetch)
+
+// const newCustomer = (customerData) => {
+//   customerData.sku = 'CYBERC_A'
+//   Authorization: 'Bearer gYEt1ZbJmcXAnB8Vtpe50KymwMu1BNLFcTV0lRB3u97HOXz4GR'
+//   Content-Type: 'application/json'
+//   'X-AdvancedCyberSecurity-SellerId': 'acs-isgro'
+//    body: JSON.stringify(customerData)
+// }
+//
+//   axios({
+//     method: 'POST',
+//     url: config.apiUrl,
+//     data: {
+//       newCustomer
+//     }
+//   })
+// .then(data=>console.log(data))
+// .catch(err=>console.log(err))
 
 module.exports = {
   newCustomer
